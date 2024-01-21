19b3ac742122a0f60dee125f630d0e5b65176395
L0giXX <marc.mueller04@gmail.com>
2024-01-21 20:03:00 +0100

    validation function

--- src/controllers/employee.ts
+++ src/controllers/employee.ts
@@ -1,20 +1,10 @@
 import { prisma } from "@utils/prisma";
 import type { Request, Response } from "express";
-import { employeeSchema } from "@utils/types";
-import type { Employee } from "@utils/types";
+import { employeeSchema, type Employee } from "@utils/types";
+import validation from "@utils/validation";
 
 const createEmployee = async (req: Request, res: Response) => {
-  const employee: Employee = req.body;
-  // const result = employeeSchema.safeParse({
-  //   body,
-  // });
-
-  // if (!result.success) {
-  //   res.status(400).json(body);
-  //   return;
-  // }
-
-  // const employee = result.data;
+  const employee = validation(employeeSchema)(req, res) as Employee;
 
   const newEmployee = await prisma.employee.create({
     data: {
